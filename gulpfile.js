484b1e971f2716f21e1b7772182af4f8353584ed
Marcio Toledo <mntoledo@gmail.com>
2017-09-07 06:20:16 -0300

    Part of assets and entire HTML done.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,30 @@ const gulp = require('gulp'),
       postcss = require('gulp-postcss'),
       autoprefixer = require('autoprefixer'),
       cssnano = require('cssnano'),
+      image = require('gulp-image'),
       del = require('del'),
       runSequence = require('run-sequence'),
       watch = require('gulp-watch'),
       livereload = require('gulp-livereload');
 
 gulp.task('clean', () =>
-  del('./public')
+  del(
+    './public/assets/css',
+    './public/assets/js',
+    './public/*.html'
+  )
 );
 
 gulp.task('pages', () =>
   gulp.src('./_pages/*.html')
     .pipe(gulp.dest('./public'))
     .pipe(livereload())
+);
+
+gulp.task('image', () =>
+  gulp.src('./_assets/images/**/*')
+    .pipe(image())
+    .pipe(gulp.dest('./public/assets/img'))
 );
 
 gulp.task('sass',function () {
@@ -39,6 +50,7 @@ gulp.task('build', function(done) {
 gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('./_assets/styles/**/*.scss', ['sass']);
+  gulp.watch('./_assets/images/*', ['image']);
   gulp.watch('./_pages/*.html', ['pages']);
 });
 
