9938ea6a2b3f5c6a2f91c95e05ee74ea11149811
Marcio Toledo <mntoledo@gmail.com>
2017-10-03 16:56:04 -0300

    Add JS, jQuery and Slick script to project, Section Testimonials, Steps and some more stuff

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -5,3 +5,13 @@ $(window).on('scroll', function() {
         $('.header').removeClass('is-scrolling');
     }
 });
+
+$('.js-testimonials').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
